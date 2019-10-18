790709be93c086ae0a125306712fd92ef211a07d
jacovanwijk <jacovwijk@hotmail.com>
2019-10-18 11:07:54 +0200

    Add eslint

--- server.js
+++ server.js
@@ -3,31 +3,33 @@ const express = require('express');
 const app = express();
 const port = 2020;
 
-app.get('/articles', (req,res) => {
-    //TODO retrieve all articles
-    res.send('Retrieved all articles');
+app.get('/', (req, res) => {
+  res.send('This server is used to retrieve articles');
+});
+
+app.get('/articles', (req, res) => {
+  // TODO retrieve all articles
+  res.send('Retrieved all articles');
 });
 
 app.get('/articles/:id', (req, res) => {
-    //TODO retrieve specific article
-    const id = req.params.id;
-    res.send(`Retrieve specific article with id: ${id}`);
-})
+  // TODO retrieve specific article
+  const id = req.params.id;
+  res.send(`Retrieve specific article with id: ${id}`);
+});
 
 app.put('/articles/:id', (res, req) => {
-    //TODO update article with specific ID
-    const id = req.params.id;
+  // TODO update article with specific ID
 });
 
 app.post('/articles', (res, req) => {
-    //TODO upload post with specific ID
+  // TODO upload post with specific ID
 });
 
 app.delete('/articles/:id', (req, res) => {
-    //TODO delete article
-    const id = req.params.id;
+  // TODO delete article
 });
 
 app.listen(port, () => {
-    console.log(`server is listening on port ${port}`);
+  console.log(`server is listening on port ${port}`);
 });
